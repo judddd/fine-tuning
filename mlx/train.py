33e38e879aea1ae05703f97a72cfacc49164300e
judddd <7714655+judddd@user.noreply.gitee.com>
2025-11-18 09:26:05 +0800

    v2

--- mlx/train.py
+++ mlx/train.py
@@ -52,15 +52,15 @@ config = {
     # 数据目录
     "data": "../dataset",
     
-    # LoRA 参数
-    "num_layers": 35,  # 改名：lora_layers -> num_layers
-    "lora_rank": 8,
-    "lora_alpha": 16, # 小模型设置scale=1防止过拟合
-    "lora_dropout": 0.0, # 设置0.0容易死记硬背
+    # LoRA 参数 20,16,32,0.1,6,200 大概400g
+    "num_layers": 20,  # 改名：lora_layers -> num_layers
+    "lora_rank": 16,
+    "lora_alpha": 32, # 小模型设置scale=1防止过拟合
+    "lora_dropout": 0.1, # 小模型设置0.05防止过拟合，增加泛化能力
     
     # 训练参数
-    "batch_size": 16,
-    "iters": 1000, # （iters*batch/训练数据量=想要训练轮数） 总训练样本数 = 18750 × 16 = 300,000 个样本，如果你的数据只有 1000 条，每条会被训练 300 次
+    "batch_size": 6,
+    "iters": 200, # （iters*batch/训练数据量=想要训练轮数） 总训练样本数 = 18750 × 16 = 300,000 个样本，如果你的数据只有 1000 条，每条会被训练 300 次
     "learning_rate": 5e-5,
     "max_seq_length": 2048,
     
